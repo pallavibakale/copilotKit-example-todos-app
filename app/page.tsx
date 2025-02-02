1ee8598a09a871f1becd08c937db36de024ec367
pallavibakale <pallavibakale09@gmail.com>
2025-02-02 01:16:09 -0500

    added copiltkitactions and chat suggestions

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,19 @@
 
 import { TasksList } from "@/components/TasksList";
 import { TasksProvider } from "@/lib/hooks/use-tasks";
+import { CopilotKit } from "@copilotkit/react-core";
+import { CopilotPopup } from "@copilotkit/react-ui";
+import "@copilotkit/react-ui/styles.css";
 
 export default function Home() {
   return (
     <>
-      <TasksProvider>
-        <TasksList />
-      </TasksProvider>
+      <CopilotKit publicApiKey="ck_pub_2dd697c66097f0e76b9584b861082d7e">
+        <TasksProvider>
+          <TasksList />
+        </TasksProvider>
+        <CopilotPopup />
+      </CopilotKit>
     </>
   );
 }
--- lib/hooks/use-tasks.tsx
+++ lib/hooks/use-tasks.tsx
@@ -1,6 +1,8 @@
 import { createContext, useContext, useState, ReactNode } from "react";
 import { defaultTasks } from "../default-tasks";
 import { Task, TaskStatus } from "../tasks.types";
+import { useCopilotReadable, useCopilotAction } from "@copilotkit/react-core";
+import { useCopilotChatSuggestions } from "@copilotkit/react-ui";
 
 let nextId = defaultTasks.length + 1;
 
@@ -15,6 +17,72 @@ const TasksContext = createContext<TasksContextType | undefined>(undefined);
 
 export const TasksProvider = ({ children }: { children: ReactNode }) => {
   const [tasks, setTasks] = useState<Task[]>(defaultTasks);
+
+  useCopilotReadable({
+    description: "The state of tasks",
+    value: JSON.stringify(tasks),
+  });
+
+  useCopilotAction({
+    name: "createTask",
+    description: "Adds a task to the todo list",
+    parameters: [
+      {
+        name: "title",
+        type: "string",
+        description: "The title of the task to create",
+        required: true,
+      },
+    ],
+    handler: ({ title }) => {
+      addTask(title);
+      return "Task created successfully";
+    },
+  });
+
+  useCopilotAction({
+    name: "deleteTask",
+    description: "Deletes a task from the todo list",
+    parameters: [
+      {
+        name: "id",
+        type: "number",
+        description: "The id of the task to delete",
+        required: true,
+      },
+    ],
+    handler: ({ id }) => {
+      deleteTask(id);
+      return "Task deleted successfully";
+    },
+  });
+
+  useCopilotAction({
+    name: "setTaskStatus",
+    description: "Sets the status of a task",
+    parameters: [
+      {
+        name: "id",
+        type: "number",
+        description: "The id of the task to update",
+        required: true,
+      },
+      {
+        name: "status",
+        description: "The status to set the task to",
+        required: true,
+        enum: Object.values(TaskStatus),
+      },
+    ],
+    handler: ({ id, status }) => {
+      setTaskStatus(id, status as TaskStatus);
+      return "Task status updated successfully";
+    },
+  });
+
+  useCopilotChatSuggestions({
+    instructions: "Try creating a task with the title 'Buy milk'",
+  });
 
   const addTask = (title: string) => {
     setTasks([...tasks, { id: nextId++, title, status: TaskStatus.todo }]);
